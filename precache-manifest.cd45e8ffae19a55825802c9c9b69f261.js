79d08a525292a171e23da027ef5f0a5641ac49ca
Android <mooh89@gmail.com>
2018-11-02 19:24:05 +0300

    Updates

---
+++ precache-manifest.cd45e8ffae19a55825802c9c9b69f261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf46e34572334003c25",
+    "url": "/mooh8989.github.io/contactmanager/static/js/runtime~main.3cf46e34.js"
+  },
+  {
+    "revision": "ab2241e4c050370d2486",
+    "url": "/mooh8989.github.io/contactmanager/static/js/main.ab2241e4.chunk.js"
+  },
+  {
+    "revision": "4963ff256a7b73fec28b",
+    "url": "/mooh8989.github.io/contactmanager/static/js/1.4963ff25.chunk.js"
+  },
+  {
+    "revision": "ab2241e4c050370d2486",
+    "url": "/mooh8989.github.io/contactmanager/static/css/main.8c73bb84.chunk.css"
+  },
+  {
+    "revision": "4963ff256a7b73fec28b",
+    "url": "/mooh8989.github.io/contactmanager/static/css/1.4882ffcf.chunk.css"
+  },
+  {
+    "revision": "7f52d221fbce1f6d56c29bdb7de757af",
+    "url": "/mooh8989.github.io/contactmanager/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mooh8989.github.io/contactmanager/precache-manifest.cd45e8ffae19a55825802c9c9b69f261.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mooh8989.github.io/contactmanager/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
